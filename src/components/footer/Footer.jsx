b45e7031a00de96b3964115dfa6551acc0b5df01
Acolytes <90561917+ProcessedCarbon@users.noreply.github.com>
2023-07-04 01:05:52 +0800

    Removed instagram

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,24 +8,24 @@ import {BsLinkedin} from 'react-icons/bs'
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className="footer_logo">GREG</a>
+      {/* <a href="#" className="footer_logo">GREG</a> */}
 
       <ul className="permalinks">
         <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#contact">Contact</a></li>
+        {/* <li><a href="#contact">Contact</a></li> */}
       </ul>
 
       <div className='footer_socials'>
-        <a href="https://www.instagram.com/gregory_w0ng/"><AiOutlineInstagram/></a>
+        {/* <a href="https://www.instagram.com/gregory_w0ng/"><AiOutlineInstagram/></a> */}
         <a href="https://www.linkedin.com/in/gregory-wong-9692b115a/"><BsLinkedin/></a>
         {/* <a href="https://www.tiktok.com/@gregorywng"><FaTiktok/></a> */}
       </div>
-      <div className="footer_copyright">
+      {/* <div className="footer_copyright">
         <small>&copy; GREG. All rights reserved.</small>
-      </div>
+      </div> */}
     </footer>
   )
 }
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -13,10 +13,10 @@ const HeaderSocials = () => {
           <a href="https://www.linkedin.com/in/gregory-wong-9692b115a/" target="_blank" rel="noreferrer" className='linkedin-icon'><BsLinkedin/></a>
           <h5 className='tool-tip'>LinkedIn</h5>
         </div>
-        <div className="instagram">
+        {/* <div className="instagram">
           <a href="https://www.instagram.com/gregory_w0ng/" target="_blank" rel="noreferrer" className='instagram-icon'><AiOutlineInstagram/></a>
           <h5 className='tool-tip'>Instagram</h5>
-        </div>
+        </div> */}
         {/* <div className="tiktok">
           <a href="https://www.tiktok.com/@gregorywng" target="_blank" className='tiktok-icon'><FaTiktok/></a>
           <h5 className='tool-tip'>TikTok</h5>
