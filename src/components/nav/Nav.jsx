958244f06acbe83c5a6a47789f7d79e998ee80b2
Acolytes <90561917+ProcessedCarbon@users.noreply.github.com>
2023-07-04 01:05:29 +0800

    Nav behaviour change

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,7 +4,7 @@ import {AiOutlineHome} from 'react-icons/ai'
 import {AiOutlineUser} from 'react-icons/ai'
 import {BiBookAlt} from 'react-icons/bi'
 import {AiOutlineFolderOpen} from 'react-icons/ai'
-import {BsPhone} from 'react-icons/bs'
+// import {BsPhone} from 'react-icons/bs'
 
 export const ThemeContext = React.createContext()
 
@@ -14,7 +14,10 @@ const Nav = () => {
     console.log(window.scrollY)
     if(window.innerWidth <= 1024 && window.innerWidth > 600){     
       {/* Tablet support */}
-      if(window.scrollY >= 660 && window.scrollY < 1938){
+      if(window.scrollY < 660){
+        setActiveNav('#');
+      }
+      else if(window.scrollY >= 660 && window.scrollY < 1938){
         setActiveNav('#about');
       }
       else if(window.scrollY >= 1938 && window.scrollY < 3106){
@@ -23,16 +26,16 @@ const Nav = () => {
       else if(window.scrollY >= 3106 && window.scrollY < 4190){
         setActiveNav('#portfolio');
       }
-      else if(window.scrollY >= 4190){
-        setActiveNav('#contact');
-      }    
-      else{
-        setActiveNav('#');
-      }
+      // else if(window.scrollY >= 4190){
+      //   setActiveNav('#contact');
+      // }    
     }
     else if(window.innerWidth <= 600){
       {/* Tablet support */}
-      if(window.scrollY >= 680 && window.scrollY < 2075){
+      if(window.scrollY < 680){
+        setActiveNav('#');
+      }
+      else if(window.scrollY >= 680 && window.scrollY < 2075){
         setActiveNav('#about');
       }
       else if(window.scrollY >= 2075 && window.scrollY < 3117){
@@ -41,15 +44,15 @@ const Nav = () => {
       else if(window.scrollY >= 3117 && window.scrollY < 4507){
         setActiveNav('#portfolio');
       }
-      else if(window.scrollY >= 4507){
-        setActiveNav('#contact');
-      }    
-      else{
-        setActiveNav('#');
-      }
+      // else if(window.scrollY >= 4507){
+      //   setActiveNav('#contact');
+      // }    
     }
     else{
-      if(window.scrollY >= 550 && window.scrollY < 1480){
+      if(window.scrollY < 550){
+        setActiveNav('#');
+      }
+      else if(window.scrollY >= 550 && window.scrollY < 1480){
         setActiveNav('#about');
       }
       else if(window.scrollY >= 1480 && window.scrollY < 2262){
@@ -58,12 +61,9 @@ const Nav = () => {
       else if(window.scrollY >= 2262 && window.scrollY < 3000){
         setActiveNav('#portfolio');
       }
-      else if(window.scrollY >= 3000){
-        setActiveNav('#contact');
-      }    
-      else{
-        setActiveNav('#');
-      }
+      // else if(window.scrollY >= 3000){
+      //   setActiveNav('#contact');
+      // }    
     }
   }, [window.scrollY])
   
@@ -73,7 +73,7 @@ const Nav = () => {
       <a href="#about" onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
       <a href="#experience" onClick={()=> setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBookAlt/></a>
       <a href="#portfolio" onClick={()=> setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><AiOutlineFolderOpen/></a>
-      <a href="#contact" onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BsPhone/></a>
+      {/* <a href="#contact" onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BsPhone/></a> */}
     </nav>
   )
 }
